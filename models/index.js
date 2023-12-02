2b23d4694431965e06c56e5e52cff36fca90d517
Harshjain3007 <hjain3007@gmail.com>
2023-12-02 16:41:34 +0530

    connected to mysql database

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize')
+const env = require('../env')
+
+const sequelize = new Sequelize(env.db_name, env.db_username, env.db_password, {
+    host: 'localhost',
+    dialect:'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+  })
+
+
+  try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
