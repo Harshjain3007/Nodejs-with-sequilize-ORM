618a562f0d7659435d8c8c5495a57ba05c50ebe9
Harshjain3007 <hjain3007@gmail.com>
2023-12-02 19:30:03 +0530

    model type 2 done

--- models/user.js
+++ models/user.js
@@ -1,27 +1,28 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes,Model } = require('sequelize');
 const sequelize = require('./index')
 
 
-const User = sequelize.define('User', {
+class User extends Model {}
+
+User.init({
   // Model attributes are defined here
   firstName: {
     type: DataTypes.STRING,
     allowNull: false
   },
   lastName: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING
     // allowNull defaults to true
-    defaultValue:"Kumar"
   }
 }, {
   // Other model options go here
-
-  tableName: 'Users',
- // timestamps:false,
-  createdAt:false,
-  updatedAt:'updated_at'
+  sequelize, // We need to pass the connection instance
+  modelName: 'User' // We need to choose the model name
 });
 
+// the defined model is the class itself
 console.log(User === sequelize.models.User); // true
+
+
 
 module.exports = User
